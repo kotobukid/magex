7c02bde8ea3c26ef6e3de5ea6044564be07cf210
kotobukid <kotobuki@daqua.jp>
2022-03-31 10:47:11 +0900

    Ajaxで生成をトリガー

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const child_process = require('child_process');
+const path = require('path');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+const random_integer = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+};
+
+router.post('/generate.json', function (req, res, next) {
+    const s = req.settings;
+    const options = [
+        `--config=${s.MAG_DIR}\\lookback_rnn`,
+        `--bundle_file=${s.MAG_DIR}\\basic_rnn.mag`,
+        `--output_dir=outputs\\magenta_music\\lookback5`,
+        `--num_output=40`,
+        `--num_steps=32`,
+        `--primer_melody="[${random_integer(57, 69)}]"`
+    ];
+    console.log('start generate');
+    console.log(options.join(' '));
+
+    child_process.exec(`${path.join(req.settings.MAGENTA_PATH, 'melody_rnn_generate.exe')} ${options.join(' ')}`, (err, stdout, stderr) => {
+        if (err) {
+            res.json({success: false});
+            return;
+        }
+        res.json({success: true});
+    })
 });
 
 module.exports = router;
